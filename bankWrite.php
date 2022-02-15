efc79dabe6b133008995461d47d844f1f27e4186
Vismay Patel <vismaypikachu@gmail.com>
2022-02-15 13:21:00 -0800

    test

--- bankWrite.php
+++ bankWrite.php
@@ -21,56 +21,61 @@
 	
 
 <?php
-
+	#variables
 	$bank = $_POST['banks'];
-	
+	for(int i = 0; i < 10; i++){
+		
+	}
+		
 	if(empty($bank)){
 		$bank = "northwestHarvest";
 		$db_name = "northwestHarvest";
 	}
-
 	$db_name = $bank;
-	
-	
-	$host = "ibcasserver.mysql.database.azure.com";
-    $username = "ibcasvismay@ibcasserver";
-    $password = "jointechsavvyyouth1!";
-
-
-    $conn = mysqli_init();
-    mysqli_real_connect($conn, $host, $username, $password, $db_name, 3306);
-
-    if (mysqli_connect_errno()) {
-      	die("Failed to connect to MYSQL: ".mysqli_connect_error());
-    }
 		
-           
-    $sql = "SELECT id, name, quantity FROM inventory";
-	$result = $conn->query($sql);
-	if($result->num_rows > 0){
-		$i = 1;
-		while ($row = $result->fetch_assoc()) {
-			echo "<tr>
+		
+	$host = "ibcasserver.mysql.database.azure.com";
+	$username = "ibcasvismay@ibcasserver";
+	$password = "jointechsavvyyouth1!";
+
+
+	$conn = mysqli_init();
+	mysqli_real_connect($conn, $host, $username, $password, $db_name, 3306);
+	if (mysqli_connect_errno()) {
+		die("Failed to connect to MYSQL: ".mysqli_connect_error());
+	}
+			
+	if(!empty($requiredCheckmark)){
+
+	}
+	else{
+		$sql = "SELECT id, name, quantity FROM inventory";
+		$result = $conn->query($sql);
+		if($result->num_rows > 0){
+			$i = 1;
+			while ($row = $result->fetch_assoc()) {
+				echo "<tr>
 					<td> <input type=\"checkbox\" id=\"check\".$i name=\"check\".$i> </td>
 					<td>". $row["id"] ."</td>
 					<td>". $row["name"] ."</td>
 					<td>". $row["quantity"] . "</td>
 					<td> <input type=\"text\" id=\"quantity\".$i name=\"quantity\".$i> </td>
 				</tr>";
-			
-			#<td> <label for="item1">Bananas</label> </td>
-			#<td> <input type="text" id="item1"> </td>
-			$i++;
+					
+				#<td> <label for="item1">Bananas</label> </td>
+				#<td> <input type="text" id="item1"> </td>
+				$i++;
+			}
+				echo "</table> <form action = \"bankWrite.php\" method = \"POST\"> 
+				<input type=\"checkbox\" id=\"requiredCheck\" name=\"requiredCheck\" required> 
+				<label for=\"requiredCheck\">I have reviewed my changes</label> </form>
+				<input type=\"submit\" value=\"Submit\">";
+			}
+			else{
+				echo "0 results availible at this time";
+			}
 		}
-		echo "</table> <form action = \"bankWrite.php\" method = \"POST\"> 
-		<input type=\"checkbox\" id=\"check\".$i name=\"check\".$i required> 
-		<label for=\"check.$i\">I have reviewed my changes</label> </form>
-		<input type=\"submit\" value=\"Submit\">";
-    }
-    else{
-    	echo "0 results availible at this time";
-    }
-
-    $conn->close();
+	}
+	$conn->close();
 ?>
 </html>
