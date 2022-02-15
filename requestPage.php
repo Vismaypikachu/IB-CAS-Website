35068de4d11d929c36f0a6314ab24421dc0a979c
Vismay Patel <vismaypikachu@gmail.com>
2022-02-14 18:27:08 -0800

    update

--- requestPage.php
+++ requestPage.php
@@ -8,37 +8,35 @@
 </head>
 <body>
 
-        <form id = "bankSelector">
-                <label for="banks">Choose a Donation Center:</label>
+	<form id = "bankSelector">
+		<label for="banks">Choose a Donation Center:</label>
+		<select id="banks" name="banks">
+			<option value="Northwest Harvest">Northwest Harvest</option>
+		</select>
+		<input type="submit" value="Submit">
+	</form>
 
-                <select id="banks" name="banks">
-                        <option value="Northwest Harvest">Northwest Harvest</option>
-                </select>
-                <input type="submit" value="Submit">
-        </form>
+    <table>
+        <tr>
+        	<th>ID</th>
+            <th>Name</th>
+        	<th>Quantity</th>
+        </tr>
+    	<?php
+            require "db.php";
+            $sql = "SELECT id, name, quantity FROM inventory";
+            $result = $conn->query($sql);
+            if($result->num_rows > 0){
+            while ($row = $result->fetch_assoc()) {
+                echo "<tr><td>". $row["id"] ."</td><td>". $row["name"] ."</td><td>". $row["quantity"] . "</td></tr>";
+            }
+            }
+            else{
+                echo "0 results availible at this time";
+            }
 
-        <table>
-                <tr>
-                        <th>ID</th>
-                        <th>Name</th>
-                        <th>Quantity</th>
-                </tr>
-                <?php
-                        require "db.php";
-                        $sql = "SELECT id, name, quantity FROM inventory";
-                        $result = $conn->query($sql);
-
-                        if($result->num_rows > 0){
-                                while ($row = $result->fetch_assoc()) {
-                                        echo "<tr><td>". $row["id"] ."</td><td>". $row["name"] ."</td><td>". $row["quantity"] . "</td></tr>";
-                                }
-                        }
-                        else{
-                                echo "0 results availible at this time";
-                        }
-
-                        $conn->close();
-                ?>
-        </table>
+            $conn->close();
+    	?>
+	</table>
 </body>
 </html>
