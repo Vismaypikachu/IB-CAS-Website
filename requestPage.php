359ac1995e0e223a173a5fd303b70beafd1c29a1
Raymon Zhang <raymonzhang.rz@gmail.com>
2022-02-14 18:01:00 -0800

    Update requestPage.php

--- requestPage.php
+++ requestPage.php
@@ -8,47 +8,37 @@
 </head>
 <body>
 
-	<form id = "bankSelector">
-		<label for="banks">Choose a Donation Center:</label>
-		
-		<select id="banks" name="banks">
-			<option value="Northwest Harvest">Northwest Harvest</option>
-		</select>
-		<input type="submit" value="Submit">
-	</form>
-    
-	<table>
-		<tr>
-			<th>ID</th>
-			<th>Name</th>
-			<th>Quantity</th>
-		</tr>
-		<?php
-			$con=mysqli_init(); 
-			mysqli_ssl_set($con, NULL, NULL, {ca-cert filename}, NULL, NULL); 
-			mysqli_real_connect($con, "ibcasserver.mysql.database.azure.com", "ibcasvismay@ibcasserver", "jointechsavvyyouth1!", "northwestHarvest", 3306);
-			#$('#searchbar').val()
-			#$conn = mysqli_connect("ibcasserver.mysql.database.azure.com", "ibcasvismay@ibcasserver", "jointechsavvyyouth1!", "northwestHarvest");
-			
-			if($conn->connect_error){
-				die("Connection Failed: " . $conn->connnect_error);
-			}
-			
-			$sql = "SELECT id, name, quantity FROM inventory";
-			$result = $conn->query($sql);
-			
-			if($result->num_rows > 0){
-				while ($row = $result->fetch_assoc()) {
-					echo "<tr><td>". $row["id"] ."</td><td>". $row["name"] ."</td><td>". $row["quantity"] . "</td></tr>";
-				}
-				echo "</table>";
-			}
-			else{
-				echo "0 results availible at this time";
-			}
-			
-			$conn->close();
-		?>
-	</table>
+        <form id = "bankSelector">
+                <label for="banks">Choose a Donation Center:</label>
+
+                <select id="banks" name="banks">
+                        <option value="Northwest Harvest">Northwest Harvest</option>
+                </select>
+                <input type="submit" value="Submit">
+        </form>
+
+        <table>
+                <tr>
+                        <th>ID</th>
+                        <th>Name</th>
+                        <th>Quantity</th>
+                </tr>
+                <?php
+                        require "db.php";
+                        $sql = "SELECT id, name, quantity FROM inventory";
+                        $result = $conn->query($sql);
+
+                        if($result->num_rows > 0){
+                                while ($row = $result->fetch_assoc()) {
+                                        echo "<tr><td>". $row["id"] ."</td><td>". $row["name"] ."</td><td>". $row["quantity"] . "</td></tr>";
+                                }
+                        }
+                        else{
+                                echo "0 results availible at this time";
+                        }
+
+                        $conn->close();
+                ?>
+        </table>
 </body>
 </html>
