b6c9298c9db1a3caeee0748217f63bc24f6dda98
Vismay Patel <vismaypikachu@gmail.com>
2022-02-14 22:03:21 -0800

    test

--- connect.php
+++ connect.php
@@ -26,9 +26,11 @@
     $sql = "SELECT id, name, quantity FROM inventory";
 	$result = $conn->query($sql);
 	if($result->num_rows > 0){
+		echo "<table>";
 		while ($row = $result->fetch_assoc()) {
 			echo "<tr><td>". $row["id"] ."</td><td>". $row["name"] ."</td><td>". $row["quantity"] . "</td></tr>";
 		}
+		echo "</table>";
     }
     else{
     	echo "0 results availible at this time";
