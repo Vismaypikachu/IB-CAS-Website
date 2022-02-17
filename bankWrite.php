7cecc045db19833b7ce395dbd0773ba31216b760
Vismay Patel <vismaypikachu@gmail.com>
2022-02-17 14:05:13 -0800

    test

--- bankWrite.php
+++ bankWrite.php
@@ -77,11 +77,10 @@
 		$num_rows = $result->num_rows;
 		if($num_rows > 0){
 			$i = 1;
-			while($i < $num_rows){
+			while($i <= $num_rows){
 				echo $_POST['quantity'.$i];
+				$result2 = $conn->query("UPDATE inventory SET quantity = " .$_POST['item'.$i]. " WHERE name = '" .$row["id"]."'");
 				$i++;
-				#$sql2 = "UPDATE inventory SET quantity = " .$_POST['item'.$i]. " WHERE name = '" .row["id"]."';";
-				#$result2 = $conn->query($sql);
 			}
 		}
 		else{
