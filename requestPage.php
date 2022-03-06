6024df2f5e5f521a865cbb8c036c983dea1444c3
Vismay Patel <vismaypikachu@gmail.com>
2022-03-06 15:48:40 -0800

    adding header

--- requestPage.php
+++ requestPage.php
@@ -7,6 +7,41 @@
 	<link rel="stylesheet" href="requestPage.css">
     <title>Request Page</title>
 </head>
+
+<!-- HEADER CONTENT -->
+<header id = "header">
+		<div id="titleBar"> <!-- Title Bar-->
+			<h1>IB CAS Project</h1>
+		</div>
+		
+		<div id="navBar"> <!-- Nav Bar-->
+			<p>
+				<a href = "requestPage.php">Request Page</a>
+				<a href = "adminPage.php">Admin Page</a>      
+				<a href="logout.php">Log Out</a>
+
+			</p>
+		</div>
+	</header>
+
+<script>
+	window.onscroll = function() {myFunction()};
+
+	// Get the header
+	var header = document.getElementById("header");
+
+	// Get the offset position of the navbar
+	var sticky = header.offsetTop;
+
+	// Add the sticky class to the header when you reach its scroll position. Remove "sticky" when you leave the scroll position
+	function myFunction() {
+	  if (window.pageYOffset > sticky) {
+		header.classList.add("sticky");
+	  } else {
+		$(".ghost").addClass("hidden");
+	  }
+	}
+</script>
 <body>
 
 	<form id = "bankSelector" action = "bankRequest.php" method = "POST">
